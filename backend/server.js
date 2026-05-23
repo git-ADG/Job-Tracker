d2fe19216a2169a0fa05bf322b1fc49be8d5b1de
git-ADG <adg8503@gmail.com>
2026-05-23 17:39:25 +0530

    FEAT: CRUD operations for applications

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,15 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const PORT = process.env.PORT || 5000;
+
+const applicationsRoutes = require('./routes/applications-routes');
 
 dotenv.config();
 
 app.use(cors());
 app.use(bodyParser.json());
-
-PORT = process.env.PORT || 5000;
+app.use('/api/applications', applicationsRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('Connected to MongoDB'))
