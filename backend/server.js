b7e6b1a725f8098b72e5c204437f920cc581f575
git-ADG <adg8503@gmail.com>
2026-05-23 18:35:06 +0530

    FIX: integrated job routes in server.js

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,15 @@ const mongoose = require('mongoose');
 const PORT = process.env.PORT || 5000;
 
 const applicationsRoutes = require('./routes/applications-routes');
+const jobRoutes = require('./routes/job-routes');
+
 
 dotenv.config();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/api/applications', applicationsRoutes);
+app.use('/api/jobs', jobRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('Connected to MongoDB'))
